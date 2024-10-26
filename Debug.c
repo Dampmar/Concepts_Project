bc0ca1aa331a5e9d4b0644935cf3f85185bb9d22
Dampmar <dampmarin@gmail.com>
2024-10-26 17:34:15 -0600

    Changes

--- Debug.c
+++ Debug.c
@@ -7,6 +7,23 @@
 FILE *inputFile;
 char token;
 
+<<<<<<< Updated upstream:Debug.c
+=======
+typedef struct {
+    char root[3];       // D, Db or D#
+    char qual;          // -, +, o, none 
+    int sus;            // sus2 or sus4
+    char ext_type;      // ^ or none 
+    int extension;      // 7, 9, 11, 13
+    char bass[3];       // D, Cb or C
+} Chord;
+
+typedef struct ChordNode {
+    Chord chord;
+    struct ChordNode* next;
+} ChordNode;
+
+>>>>>>> Stashed changes:ChordCalc.c
 // Function declarations 
 void error(char* message);
 void getToken();
@@ -30,12 +47,34 @@ int num();
 void sus();
 void bass();
 
+<<<<<<< Updated upstream:Debug.c
 // Main function
 int main(void){
     char filename[100];
     printf("Enter the name of the file to be parsed: ");
     scanf("%s", filename);
     inputFile = fopen(filename, "r");
+=======
+// Chord calculator functions 
+void printChord(Chord* chord);
+void printChords (ChordNode* head);
+void createChordArr(Chord* chord, int* arr);
+int noteToPitchClass(const char* root);
+
+// Main Function 
+int main(void) {
+    char filepath[365];
+    printf("Enter the path of the file to be parsed: ");
+    scanf("%[^\n]s", filepath);
+    // Checking if the file is indeed a .txt file 
+    if (strlen(filepath) < 4 || strcmp(filepath + strlen(filepath) - 4, ".txt") != 0) {
+        printf("Error: Only .txt files are supported\n");
+        exit(1);
+    }
+
+    // Opening the file to handle it 
+    inputFile = fopen(filepath, "r");
+>>>>>>> Stashed changes:ChordCalc.c
     if (inputFile == NULL){
         printf("Error opening file\n");
         exit(1);
@@ -46,6 +85,240 @@ int main(void){
     fclose(inputFile);
     return 0;
 }
+<<<<<<< Updated upstream:Debug.c
+=======
+
+// Functions for handling chords 
+ChordNode* createNode(Chord chord) {
+    ChordNode* newNode = (ChordNode*)malloc(sizeof(ChordNode));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->chord = chord;
+    newNode->next = NULL;
+    return newNode;
+}
+
+ChordNode* appendChord(ChordNode* head, Chord chord) {
+    ChordNode* newNode = createNode(chord);
+    if (head == NULL) {
+        return newNode;
+    }
+    ChordNode* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    return head;
+}
+
+void printChords(ChordNode* head) {
+    ChordNode* temp = head; 
+    int chordNum = 1;
+    int pitchClassNum[12] = {0};
+    printf("\n");
+    printf("      \t 0 \t 1 \t 2 \t 3 \t 4 \t 5 \t 6 \t 7 \t 8 \t 9 \t A \t B \t");
+    printf("\n");
+    printf("      \t - \t - \t - \t - \t - \t - \t - \t - \t - \t - \t - \t - \t");
+    printf("\n");
+    while (temp != NULL) {
+        int arr[12] = {0};
+        printf("%4d. ", chordNum);
+        createChordArr(&temp->chord, arr);  // Calculating pitch class
+        // Printing the pitch class of the chord 
+        printf("\t ");
+        for (int i = 0; i < 12; i++) {
+            if (arr[i]) {
+                printf("* \t");
+                pitchClassNum[i]++;
+            }
+            else {
+                printf("  \t");
+            }
+        }
+        printf("- ");
+        printChord(&temp->chord);
+        printf("\n");
+        temp = temp->next;
+        chordNum++;
+    }
+    printf("      \t - \t - \t - \t - \t - \t - \t - \t - \t - \t - \t - \t - \t");
+    printf("\n");
+    printf("      \t");
+    for (int i = 0; i < 12; i++) {
+        printf("%3d\t", pitchClassNum[i]);
+    }
+    printf("\n");
+}
+
+// Creating the pitch class of a chord in an array 
+void createChordArr(Chord* chord, int* arr) {
+    memset(arr, 0, sizeof(int) * 12); 
+
+    // Getting Pitch Class
+    int rootPC = noteToPitchClass(chord->root);
+    if (rootPC == -1) {
+        printf("Invalid note");
+        exit(1);
+    }
+
+    arr[rootPC] = 1; // Mark the root note
+
+    // Handle chord quality
+    switch (chord->qual) {
+        case '-':
+            arr[(rootPC + 3) % 12] = 1;
+            arr[(rootPC + 7) % 12] = 1;
+            break;
+        case '+':
+            arr[(rootPC + 4) % 12] = 1;
+            arr[(rootPC + 8) % 12] = 1;
+            break;
+        case 'o':
+            arr[(rootPC + 3) % 12] = 1;
+            arr[(rootPC + 6) % 12] = 1;
+            break;
+        default: 
+            arr[(rootPC + 4) % 12] = 1;
+            arr[(rootPC + 7) % 12] = 1;
+            break;
+    }
+    
+    // Handle chord qnum 
+    switch (chord->extension) {
+        case 7:
+            if (chord->ext_type == '^') {
+                arr[(rootPC + 11) % 12] = 1;
+            } else {
+                arr[(rootPC + 10) % 12] = 1;
+            }
+            break;
+        case 9:
+            if (chord->ext_type == '^') {
+                arr[(rootPC + 2) % 12] = 1;
+                arr[(rootPC + 11) % 12] = 1;
+            } else {
+                arr[(rootPC + 2) % 12] = 1;
+                arr[(rootPC + 10) % 12] = 1;
+            }
+            break;
+        case 11:
+            if (chord->ext_type == '^') {
+                arr[(rootPC + 5) % 12] = 1;
+                arr[(rootPC + 11) % 12] = 1;
+            } else {
+                arr[(rootPC + 5) % 12] = 1;
+                arr[(rootPC + 10) % 12] = 1;
+            }
+            break;
+        case 13:
+            if (chord->ext_type == '^') {
+                arr[(rootPC + 9) % 12] = 1;
+                arr[(rootPC + 11) % 12] = 1;
+            } else {
+                arr[(rootPC + 9) % 12] = 1;
+                arr[(rootPC + 10) % 12] = 1;
+            }
+        default:
+            break;
+    }
+
+    // Handle chord suspension
+    switch (chord->sus) {
+        case 2:
+            arr[(rootPC + 2) % 12] = 1;
+            arr[(rootPC + 7) % 12] = 1;
+            break;
+        case 4:
+            arr[(rootPC + 5) % 12] = 1;
+            arr[(rootPC + 7) % 12] = 1;
+            break;
+        default:
+            break;
+    }
+
+    // Handle chord bass
+    if (chord->bass[0] != '\0') {
+        int bassPC = noteToPitchClass(chord->bass);
+        if (bassPC != -1) arr[bassPC] = 1;
+    }
+
+}
+
+int noteToPitchClass(const char* root) {
+    switch (root[0]) {
+        case 'C': 
+            if (root[1] == 'b') return 11;
+            if (root[1] == '\0') return 0;
+            if (root[1] == '#') return 1;
+        case 'D':
+            if (root[1] == 'b') return 1;
+            if (root[1] == '\0') return 2;
+            if (root[1] == '#') return 3;
+        case 'E':
+            if (root[1] == 'b') return 3;
+            if (root[1] == '\0') return 4;
+            if (root[1] == '#') return 5;
+        case 'F':
+            if (root[1] == 'b') return 4;
+            if (root[1] == '\0') return 5;
+            if (root[1] == '#') return 6;
+        case 'G':
+            if (root[1] == 'b') return 6;
+            if (root[1] == '\0') return 7;
+            if (root[1] == '#') return 8;
+        case 'A':
+            if (root[1] == 'b') return 8;
+            if (root[1] == '\0') return 9;
+            if (root[1] == '#') return 10;
+        case 'B':
+            if (root[1] == 'b') return 10;
+            if (root[1] == '\0') return 11;
+            if (root[1] == '#') return 0;
+        default:
+            return -1;
+    }
+}
+
+// Function to convert root note 
+void printChord(Chord* chord) {
+    printf("%s", chord->root);
+    // Checking for the description of this chord
+    if (chord->qual != ' ') { printf("%c", chord->qual); }                              // if it has a quality
+    if (chord->ext_type == '^') { printf("%c%d", chord->ext_type, chord->extension); }  // if it has a qnum starting with '^'
+    else if (chord->extension != 0) { printf("%d", chord->extension); }                 // if it has a qnum with no '^'
+    if (chord->sus != 0) { printf("sus%d", chord->sus); }                               // if it has a sus 
+    // Checking for the bass of the chord
+    if (chord->bass[0] != '\0') { printf("/%s", chord->bass); }
+}
+
+void printCho(ChordNode* head) {
+    ChordNode* temp = head;
+    while (temp != NULL) {
+        printf("Chord: %s", temp->chord.root);
+        if (temp->chord.qual != ' ') printf("%c", temp->chord.qual);
+
+        if (temp->chord.ext_type == '^') printf("%c%d", temp->chord.ext_type, temp->chord.extension);
+        else if (temp->chord.extension != 0) printf("%d", temp->chord.extension);
+
+        if (temp->chord.sus != 0) printf("sus%d", temp->chord.sus);
+
+        if (temp->chord.bass[0] != '\0') printf(" / %s\n", temp->chord.bass);
+        else printf("\n");
+        temp = temp->next;
+    }
+}
+
+void freeChords(ChordNode* head) {
+    ChordNode* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+>>>>>>> Stashed changes:ChordCalc.c
 
 // Program functions
 void error(char* message) {
@@ -287,7 +560,12 @@ void qnum(){
     if (token == '^') {
         temp = token;
         match('^', "^ expected");
+<<<<<<< Updated upstream:Debug.c
     }
+=======
+    } 
+    chord->ext_type = temp;
+>>>>>>> Stashed changes:ChordCalc.c
     int x = num();
     printf("Qnum: %c %d\n", temp, x);
     // printf("Debug: qnum: Finished qnum parsing\n");
